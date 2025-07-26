965d38e7462634a77dd7bc0979fed0b8e946a3ee
madhur-j-10 <madhurj262@gmail.com>
2025-07-26 08:30:14 +0530

    Initial commits

---
+++ src/main/java/com/system/payment_reminder_system/PaymentReminderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.payment_reminder_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReminderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentReminderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/payment_reminder_system/PaymentReminderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.payment_reminder_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReminderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
